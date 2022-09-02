324ba553bcd46101cf7f1bf916e361a620e5fedf
huchiwen <clot48@gmail.com>
2022-09-02 16:41:49 +0800

    update file

--- seq.py
+++ seq.py
@@ -4,7 +4,6 @@
  d 等于page_num,page_num 就是每页的条数
   
 '''
-import time
 import pprint
 
 
@@ -12,32 +11,23 @@ def page_code():
 
     a1 = 1 
     page_num = 20
-    page_size = 446855
-    total = page_size / page_num
-    data = {}
+    page_size = 1001
+    #total = page_size / page_num
+    data = []
 
     for i in range(1,page_size):
 
         list_number = a1 + (i -1) * page_num
-        step =(list_number + page_num) - 1
-        data[i] = {list_number,step} 
-        data.update()
-        
-        #print(list_number,step)
+        step = (list_number + page_num) -1
+        data.append([list_number,step])
     return data
-   
-'''
-   要用遍历字典,
-   再用list才可以取到字典里面的值
 
-'''
 if __name__ == '__main__':
    
    r = page_code()
    pp = pprint.PrettyPrinter(width=41, compact=True)
    #pp.pprint(type(r))
-   for k,v in r.items():
-      list_number = list(v)[0]
-      step = list(v)[1]
-      print(k,list_number,step)
-      
+
+   for i in r :
+       print(i)
+
